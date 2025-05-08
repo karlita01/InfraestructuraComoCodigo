bf5ebe369c07a42dfff4f5a973682ce69c0220c7
VolphosDev <moralesbenitescharlie@gmail.com>
2025-05-08 17:55:21 -0500

    Actualizar rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "productos_db" {
   username             = "dbadmin123"      
   password             = "dbadmin123" 
   parameter_group_name = "default.postgres14"
-  publicly_accessible  = true
+  publicly_accessible  = false
   skip_final_snapshot  = true
 
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
