cc6d063117cc282042dbd3d7e0ac183652f4e624
VolphosDev <202454707+VolphosDev@users.noreply.github.com>
2025-06-14 15:16:15 -0500

    fix(IAC): Geo-Restricted

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -52,7 +52,8 @@ resource "aws_cloudfront_distribution" "frontend_cdn" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type = "whitelist"
+      locations        = ["PE"] # Código de Perú
     }
   }
 
