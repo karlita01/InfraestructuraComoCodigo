d856ec954319c12e53f5e31c459ff9ffdf5395a7
VolphosDev <202454707+VolphosDev@users.noreply.github.com>
2025-06-14 22:29:27 -0500

    Fix(IAC):RDS-Point-to-VPC-inside

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -51,10 +51,10 @@ resource "aws_security_group" "rds_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Permite salida a cualquier destino"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [aws_vpc.main.cidr_block]
+  description = "Permite salida solo dentro de la VPC"
   }
 }
