7d7bbf6c20464217e82164fd8966ad3f3f91d066
VolphosDev <202454707+VolphosDev@users.noreply.github.com>
2025-06-14 22:43:48 -0500

    Fix(IAC): Delete-Public-Entrance-S3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -76,25 +76,6 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
   }
 }
 
-resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.frontend.id
-
-  depends_on = [aws_s3_bucket_public_access_block.frontend_block]
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_s3_bucket" "frontend_backup" {
   bucket         = "minimarket-frontend-charlie-backup"
   force_destroy  = true
